ce809370877fd106f9c96fb3347b0089ae0b421b
yixiao.liu <yixiao.liu@boomingtech.com>
2026-04-07 20:12:20 +0800

    015-2 添加 Renderer

---
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -0,0 +1,34 @@
+#include "ImGuiLayer.h"
+
+#include "imgui.h"
+#include "Platform/OpenGL/ImGuiOpenGLRenderer.h"
+
+namespace Hazel {
+
+    ImGuiLayer::ImGuiLayer()
+        : Layer("ImGuiLayer")
+    {
+    }
+
+    ImGuiLayer::~ImGuiLayer()
+    {
+    }
+
+    void ImGuiLayer::OnAttach()
+    {
+        
+    }
+
+    void ImGuiLayer::OnDetach()
+    {
+    }
+
+    void ImGuiLayer::OnUpdate()
+    {
+    }
+    
+    void ImGuiLayer::OnEvent(Event &event)
+    {
+    }
+
+}
